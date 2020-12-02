5b2ef69a80eb96691a22515d4e35620b02a64f8c
iamraivis <rozols8@gmail.com>
2020-12-02 18:08:54 +0200

    fixed security groups for inbound traffic

--- terraform/modules/securityGroup/resource.tf
+++ terraform/modules/securityGroup/resource.tf
@@ -52,4 +52,40 @@ resource "aws_security_group" "sg_80" {
 	project     = "${var.project_tag}"
 	department  = "${var.department_tag}"
 	}
+
+resource "aws_security_group" "sg_https" {
+  name   = "sg_https"
+  vpc_id = "${var.vpc_id}"
+
+  ingress {
+    from_port    = 443
+    to_port      = 443
+    protocol     = "https"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port    = 0
+    to_port      = 0
+    protocol     = "-1"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+
+resource "aws_security_group" "sg_http" {
+  name   = "sg_http"
+  vpc_id = "${var.vpc_id}"
+
+  ingress {
+    from_port    = 80
+    to_port      = 80
+    protocol     = "http"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port    = 0
+    to_port      = 0
+    protocol     = "-1"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
 }	
